3f8bb93df0e6e6a338a87bd4da23cdef8efc97e5
PaKu <cookie4rent@gmail.com>
2019-11-04 01:10:40 +0100

    Work on userinfo gui and API

--- javascripts/userinfo-vue.js
+++ javascripts/userinfo-vue.js
@@ -1,10 +1,13 @@
-const server = 'http://localhost:8080/rest/';
+const server = 'http://localhost:8080/arbeitsplan/rest/';
 
 const app = new Vue({
   el: '#vueapp',
   data : {
+          showNewSchedule: false,
+          newschedule: null,
           editschedule : null,
-          schedules : []
+          schedules : [],
+          scheduleItems : []
   },
 
   methods: {
@@ -16,19 +19,93 @@ const app = new Vue({
                         .then( () => {
                                 console.log('dataset updated');
                         });
-        }
-  },
+        },
 
-  mounted() {
+        createNewSchedule() {
+            this.newschedule = {
+                "idSchedule" : "-1",
+                "label" : "",
+                "startdate" : "2000-01-01",
+                "enddate" : "2000-01-01"
+            }
+            this.showNewSchedule = true;
+        },
+
+      compNewScheduleOk() {
+          console.log(this.newschedule.label);
+          fetch(server + 'schedule/create.php', {
+              body : JSON.stringify(this.newschedule), 
+              method : "POST",
+              headers : { "Content-Type" : "application/json" }
+          })
+            .then( () => {
+                this.showNewSchedule = false;
+                this.newschedule = null;
+                this.fetchSchedules();
+            });
+      },
+
+      compNewScheduleAbort() {
+          this.showNewSchedule = false;
+          this.newschedule = null;
+      },
+
+      fetchSchedules() {
+
+          this.schedules = null;
+
           fetch(server + 'schedule/read.php')
+                .then( (response) => {
+                    if (response.status == 200 ) {
+                        return response;
+                    } else {
+                        throw "No data";
+                    }
+                })
                 .then(response => response.json() )
                 .then( (data) => {
                         this.schedules = data;
                 })
+                .catch( (data) => {
+                    console.log(data);
+                })
+      },
+
+      fetchScheduleItems(idSchedule) {
+            this.scheduleItems = null;
+
+          fetch(server + 'schedule_items/read.php')
+           .then( (response) => {
+                if (response.status == 200 ) {
+                    return response;
+                } else {
+                    throẃ "No data";
+                }
+           })
+          .then( (response) => response.json() )
+          .then( this.scheduleItems = response )
+          .catch( (data) => {
+              console.log(data);
+          });
+
+    }
+  },
+
+  mounted() {
+      this.fetchSchedules();
+      this.scheduleItems = null;
   },
 
         template: `
                 <div><h2>Arbeitszeiten</h2>
+                <button v-on:click="createNewSchedule()">Neuen Stundenplan hinzufügen</button>
+                <div v-if="showNewSchedule"> 
+                    <new-schedule 
+                        v-on:buttonNewScheduleOk="compNewScheduleOk()" 
+                        v-on:buttonNewScheduleAbort="compNewScheduleAbort()" 
+                        v-bind:nschedule="newschedule"> 
+                    </new-schedule>
+                </div>
                 <ul>
                         <li v-for="schedule in schedules">
 
@@ -36,7 +113,7 @@ const app = new Vue({
                                 Bezeichnung: <input v-model="schedule.label" v-on:keyup.13="updateSchedule(schedule)"> 
                                 Startdatum: <input v-model="schedule.startdate" v-on:keyup.13="updateSchedule(schedule)"> 
                                 Enddatum <input v-model="schedule.enddate" v-on:keyup.13="updateSchedule(schedule)"> 
-                                <schedule_items_list> </schedule_items_list>
+                                <schedule-items-list> </schedule-items-list>
                                 <button v-on:click="updateSchedule(schedule)" v-on:submit.prevent>Speichern</button> 
                         </div>
 
@@ -53,7 +130,22 @@ const app = new Vue({
 
 });
 
-Vue.component('schedule_items_list', {
+Vue.component('new-schedule', {
+    props : ['nschedule'],
+
+    template: `
+        <div class="new-schedule">
+            Bezeichnung: <input v-model="nschedule.label" type="text"/> <br>
+            Startdatum: <input v-model="nschedule.startdate" type="text" /> <br>
+            Enddatum: <input v-model="nschedule.enddate" type="text" /> <br>
+            <button v-on:click="$emit('buttonNewScheduleOk')">Hinzufügen</button> 
+            <button v-on:click="$emit('buttonNewScheduleAbort')">Abbrechen</button>
+        </div>
+        `
+});
+
+Vue.component('schedule-items-list', {
+        props : [ ['schedule-items'] ],
         data : function() {
                 return { 
                         count : 0
@@ -64,3 +156,43 @@ Vue.component('schedule_items_list', {
                 <div>Component foo</div>
                 `
 });
+
+Vue.component('schedule-list', {
+    props : [ ['schedules'], showNewSchedule, newschedule, editschedule ],
+    data : function() {
+        return {
+            count : 0
+        }
+    },
+    template : `
+        <div class="schedule-list">
+
+                <h2>Arbeitszeiten</h2>
+                <button v-on:click="$emit('buttonCreateNewSchedule') ">Neuen Stundenplan hinzufügen</button>
+                <div v-if="showNewSchedule"> 
+                    <new-schedule 
+                        v-on:buttonNewScheduleOk="$emit('buttonCompNewScheduleOk')" 
+                        v-on:buttonNewScheduleAbort="$emit('buttonCompNewScheduleAbort()" 
+                        v-bind:nschedule="newschedule"> 
+                    </new-schedule>
+                </div>
+                <ul>
+                        <li v-for="schedule in schedules">
+
+                        <div v-if="editschedule === schedule.idSchedule">
+                                Bezeichnung: <input v-model="schedule.label" v-on:keyup.13="updateSchedule(schedule)"> 
+                                Startdatum: <input v-model="schedule.startdate" v-on:keyup.13="updateSchedule(schedule)"> 
+                                Enddatum <input v-model="schedule.enddate" v-on:keyup.13="updateSchedule(schedule)"> 
+                                <schedule-items-list> </schedule-items-list>
+                                <button v-on:click="updateSchedule(schedule)" v-on:submit.prevent>Speichern</button> 
+                        </div>
+
+                        <div v-else>
+                                <button v-on:click="editschedule = schedule.idSchedule" v-on:submit.prevent>BEARBEITEN</button>
+                                {{schedule.label}} ({{schedule.startdate}} - {{schedule.enddate}})
+                        </div>
+                        </li>
+
+
+                </ul>
+        </div>
--- rest/schedule/read.php
+++ rest/schedule/read.php
@@ -9,9 +9,16 @@ $helper = new Helper();
 $userid = $helper->TransformUserToId($_COOKIE["username"]);
 
 $schedules = $helper->restapi_schedule_read($userid);
-http_response_code(200);
 
-echo json_encode($schedules);
+if ( count($schedules) > 0 ) {
+    http_response_code(200);
+    echo json_encode($schedules);
+} else {
+    http_response_code(204);
+    $schedules = array();
+    $schedules['msg'] = "No data";
+    echo json_encode($schedules);
+} 
 
 
 
