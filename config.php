5bbe9ef9e917efcf79caac2be56b54a1f3817996
Pascal Kuck <pascal@pkit.eu>
2021-09-16 13:24:29 +0200

    Testing env in CConfig

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php
 
+CConfig::$dbuser = empty(getenv('MYSQL_USER')) ? 'dbuser' : getenv('MYSQL_USER');
+
 class CConfig {
-	public static $dbuser = 'dbuser'; // empty(getenv('MYSQL_USER')) ? 'dbuser' : getenv('MYSQL_USER');
+	public static $dbuser; // = 'dbuser'; // empty(getenv('MYSQL_USER')) ? 'dbuser' : getenv('MYSQL_USER');
 	public static $dbpass = 'dbpass'; // (empty(getenv('MYSQL_PASSWORD'))) ? 'dbuser' : getenv('MYSQL_PASSWORD');
 	public static $dbhost = 'dbhost'; // (empty(getenv('MYSQL_HOST'))) ? 'dbuser' : getenv('MYSQL_HOST');
 	public static $dbname = 'dbname'; // (empty(getenv('MYSQL_DB'))) ? 'dbuser' : getenv('MYSQL_DB');
 	public static $jwtsecret = 'secret'; // (empty(getenv('JWTSECRET'))) ? 'dbuser' : getenv('JWTSECRET'); // Must be 12 characters in length, contain upper and lower case letters, a number, and a special character `*&!@%^#$``
 	public static $db_tbl_prefix = "aplan_";
-        public static $max_rank_workfields = 24;
+    public static $max_rank_workfields = 24;
 
 	/* Adminpanel options */
 	public static $backupdir = "backup";
