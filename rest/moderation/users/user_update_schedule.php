c6e98f95d74f81894bfe781301dd38bb1c875f67
PaKu <cookie4rent@gmail.com>
2023-02-22 09:52:04 +0100

    Bugfix API endpoint update user schedule

--- rest/moderation/users/user_update_schedule.php
+++ rest/moderation/users/user_update_schedule.php
@@ -29,10 +29,17 @@ if ( !isModerator($userid) ) {
     header("HTTP/1.1 401 Unauthorized");
 
 } else {
-    $createSchedule = $helper->restapi_schedule_update($dataInput->userId, json_encode($dataInput));
-    if ( $createSchedule == 'ok' ) {
-	header("HTTP/1.1 200 OK");
+    $updateSchedule = $helper->restapi_schedule_update($dataInput->userId, json_encode($dataInput));
+    if ( $updateSchedule == 'ok' ) {
+	     header("HTTP/1.1 200 OK");
+       $response["msg"] = "ok";
+       $response["code"] = 200;
+
     } else {
-	header("HTTP/1.1 500 APPLICATION ERROR");
+	     header("HTTP/1.1 500 APPLICATION ERROR");
+       $response["msg"] = $updateSchedule;
+       $response["code"] = 500;
     }
+
+    echo json_encode($response);
 }
