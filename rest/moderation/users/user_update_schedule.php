04750fa580f9fb07bf69c6d7c785790a19c62724
PaKu <cookie4rent@gmail.com>
2023-02-22 09:46:30 +0100

    Added API endpoint for updating schedules

---
+++ rest/moderation/users/user_update_schedule.php
@@ -0,0 +1,38 @@
+<?php
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require("../../cors.php");
+cors();
+
+require("../../jwt.php");
+$data = getJwtData();
+$userid = $data['id'];
+
+
+include_once("../../../helper.class.php");
+
+$helper = new Helper();
+
+require_once ("../../member.php");
+
+
+$dataInput = file_get_contents("php://input");
+$dataInput = json_decode($dataInput);
+
+if ( !isModerator($userid) ) {
+    header("HTTP/1.1 401 Unauthorized");
+
+} else if (!isset($dataInput->userId) ) {
+    header("HTTP/1.1 401 Unauthorized");
+
+} else {
+    $createSchedule = $helper->restapi_schedule_update($dataInput->userId, json_encode($dataInput));
+    if ( $createSchedule == 'ok' ) {
+	header("HTTP/1.1 200 OK");
+    } else {
+	header("HTTP/1.1 500 APPLICATION ERROR");
+    }
+}
