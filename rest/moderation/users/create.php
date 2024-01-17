7286a72a2bf7dd652eeb07d3288996a49fd21a6c
PaKu <cookie4rent@gmail.com>
2024-01-17 08:47:17 +0100

    output changed

--- rest/moderation/users/create.php
+++ rest/moderation/users/create.php
@@ -24,12 +24,13 @@ $dataInput = json_decode($dataInput);
 
 if ( !isModerator($userid) ) {
     header("HTTP/1.1 401 Unauthorized");
+    echo "not authorized!";
 } else {
 	$createUser = $helper->restapi_user_create(json_encode($dataInput));
+	echo json_encode($createUser);
 	if ( $createUser == 'ok' ) {
 		header("HTTP/1.1 200 OK");
 	} else {
 		header("HTTP/1.1 500 APPLICATION ERROR");
 	}
-	echo json_encode($createUser);
 }
